a87c90176329f0001662d2a50f3e622cf7bac891
Jon Boydell <jonboydell@hotmail.com>
2019-12-20 13:09:46 +0000

    added powerline fonts

--- fonts/setup.sh
+++ fonts/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p ~/projects/fonts 
 git clone git@github.com:tonsky/FiraCode.git ~/projects/fonts/firacode
+git clone https://github.com/powerline/fonts.git ~/projects/fonts/powerline
